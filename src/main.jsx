cd7eb1e4ffc5eee12a98f656915911414ebe132a
marimiss1901-star <marimiss1901@gmail.com>
2026-08-29 16:33:55 +0300

    Guard frontend render crashes: src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import AppErrorBoundary from './components/AppErrorBoundary'
 import './lib/callAuthCompat'
 import './styles/tokens.css'
 import './styles/global.css'
@@ -11,8 +12,10 @@ window.__ELISEI_API_BASE__ = (import.meta.env.VITE_API_BASE_URL || '').replace(/
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AppErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppErrorBoundary>
   </React.StrictMode>
 )
